8bdd99355347d533b4fc0f813bd812e04bb1aabe
FaiberCamachoDev <faibercamacho16@gmail.com>
2026-04-10 07:44:10 -0400

    feat: switch expressions and ¨when¨

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double salario = 2000.0;
+        System.out.println("salary: " + obtenerCategoriaSalarial(salario));
         }
+    // switch expressions
+    public static String obtenerCategoriaSalarial(double salario) {
+
+        // El "return" va directo antes del switch.
+        // Todo el bloque switch se evalúa y escupe el String final.
+        return switch (Double.valueOf(salario)) {
+
+            // Usamos la palabra 'when' para crear los rangos
+            case Double s when s < 2000.0 -> "Junior";
+
+            case Double s when s >= 2000.0 && s < 5000.0 -> "Senior"; // el when se usa para condiciones, como if-else if-else
+
+            case Double s when s >= 5000.0 -> "Expert";
+
+            // Las Switch Expressions OBLIGAN a tener un default
+            default -> "Rango no válido";
+        };
     }
+
+}
+
